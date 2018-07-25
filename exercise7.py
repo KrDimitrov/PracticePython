fb36a5d6e4e560a7f3f7829ae524c17cfe18e934
Kristiyan Dimitrov <kdimitrov.business.com>
2018-07-25 22:01:36 +0300

    Exercise 7 Print

--- exercise7.py
+++ exercise7.py
@@ -1,3 +1,1 @@
-b = [i for i in [1, 4, 9, 16, 25, 36, 49, 64, 81, 100] if i%2==0]
-"print(b)"
-"The Exercise doesnt tell us to print so..."
+print([i for i in [1, 4, 9, 16, 25, 36, 49, 64, 81, 100] if i%2==0])
